aff1f8222aa56652fc8b49f0b1bf724abdfc20ab
Almamun <riponrs14@gmail.com>
2020-03-07 09:17:47 +0600

    false vs true

---
+++ false-true.js
@@ -0,0 +1,16 @@
+//falsy
+//Empty string 
+// if value = 0
+// if value is undefined
+// if value is Null
+// if value is NaN
+// truthy
+// '0', '', []
+
+const name = "mamun";
+if(name.length > 0){
+    console.log("The conditon is true");
+}
+else{
+    console.log("The condition is false");
+}
